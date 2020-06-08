c05e9f5696a19dc5a0ade9042b9d74790579a2d7
Mabbutnem <thibault.harel1@gmail.com>
2020-06-08 19:53:00 +0200

    movements

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+   [SerializeField] private float force = 1500f;
+
+   private Vector2 target;
+   private Vector2 velocity = Vector2.zero;
+   private bool toMove = false;
+   private new Rigidbody2D rigidbody2D;
+
+   void Start()
+   {
+      rigidbody2D = GetComponent<Rigidbody2D>();
+   }
+
+   void Update()
+   {
+      if (Input.GetKey(KeyCode.Mouse0))
+      {
+         toMove = true;
+         target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      }
+   }
+
+   void FixedUpdate()
+   {
+      if(toMove)
+      {
+         toMove = false;
+         Vector2 dir = (target - (Vector2)transform.position).normalized;
+         rigidbody2D.AddForce(Time.fixedDeltaTime * force * dir);
+      }
+   }
+}
